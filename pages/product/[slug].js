0178552c1cb1f16e9ada2921ed5969cec4fab758
athavan <52910050+sundaraathavan@users.noreply.github.com>
2022-05-02 10:01:43 -0700

    master

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -51,7 +51,7 @@ export default function ProductScreen(props) {
       }
     };
     fetchData();
-  }, );
+  }, []);
 
   const addToCartHandler = async () => {
     const existItem = cart.cartItems.find((x) => x._id === product._id);
